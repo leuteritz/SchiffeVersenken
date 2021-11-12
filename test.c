91d37cc5066d338655f9765011493a9eb35ebc5f
leuteritz <93645530+leuteritz@users.noreply.github.com>
2021-11-12 13:24:29 +0100

    Add files via upload

--- test.c
+++ test.c
@@ -5,81 +5,61 @@
 #include <time.h>
 
 
+int checkNumber(int *lookup, int *size, int randomNumber)
+{
+    int counter = 0;
+    for (int i = 0; i < *size; i++)
+    {
+        if (lookup[i] == randomNumber)
+        {
+            return counter;
+        }
+        counter++;
+    }
+}
+
 int main()
 {
     srand((unsigned)time(NULL));
 
-    int size = 10;
+    int size = 9;
     int *lookup = (int *)malloc((unsigned long)size * sizeof(int));
 
     for (int i = 0; i < size; i++)
+
     {
-        lookup[i] = i;
+        lookup[i] = i + 1;
     }
-
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d\t", lookup[i]);
-    }
-
-    printf("\n");
-    printf("\n");
-
-
-    int pos = lookup[rand() % (size - 1 + 1) + 1];
-
-    printf("%d\n", pos);
-
-    printf("\n");
-
-    for (int i = pos; i < size; i++)
-    {
-        lookup[i] = lookup[i + 1];
-    }
-    size--;
 
     for (int j = 0; j < 9; j++)
-
     {
         for (int i = 0; i < size; i++)
         {
             printf("%d\t", lookup[i]);
         }
 
-        pos = lookup[rand() % (size - 1 + 1) + 1];
-
         printf("\n");
         printf("\n");
 
-        printf("%d\n", pos);
+
+        int pos = lookup[(rand() % size)];
+
+        printf("RandomNumber: %d\n", pos);
 
         printf("\n");
 
-        if (lookup[pos] == pos)
+        int a = checkNumber(lookup, &size, pos);
+
+        printf("NumberPosition: %d\n", a);
+        printf("\n");
+        printf("\n");
+        printf("\n");
+
+        for (int i = a; i < size; i++)
         {
-            for (int i = pos; i < size; i++)
-            {
-                lookup[i] = lookup[i + 1];
-            }
-            size--;
+            lookup[i] = lookup[i + 1];
         }
-        else if (lookup[pos] == pos - 1)
-        {
-            for (int i = pos - 1; i < size; i++)
-            {
-                lookup[i] = lookup[i + 1];
-            }
-            size--;
-        }
-        else
-        {
-            for (int i = pos - 1; i < size; i++)
-            {
-                lookup[i] = lookup[i + 1];
-            }
-            size--;
-        }
+        size--;
     }
 
 
