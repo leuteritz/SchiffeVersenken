1491867b779fb5f4da2ac6a4fa1ed50fa127db22
leuteritz <93645530+leuteritz@users.noreply.github.com>
2021-11-12 12:36:15 +0100

    Add files via upload

--- test.c
+++ test.c
@@ -1,87 +1,17 @@
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
-
 
 int main()
 {
+    char numberLookup[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
     srand((unsigned)time(NULL));
 
-    int size = 10;
-    int *lookup = (int *)malloc((unsigned long)size * sizeof(int));
+    int max = 9;
+    int min = 1;
 
-    for (int i = 0; i < size; i++)
-    {
-        lookup[i] = i;
-    }
+    char randomNumber = numberLookup[rand() / (RAND_MAX / (max - min + 1) + 1)];
 
-
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d\t", lookup[i]);
-    }
-
-    printf("\n");
-    printf("\n");
-
-
-    int pos = lookup[rand() % (size - 1 + 1) + 1];
-
-    printf("%d\n", pos);
-
-    printf("\n");
-
-    for (int i = pos; i < size; i++)
-    {
-        lookup[i] = lookup[i + 1];
-    }
-    size--;
-
-    for (int j = 0; j < 9; j++)
-
-    {
-        for (int i = 0; i < size; i++)
-        {
-            printf("%d\t", lookup[i]);
-        }
-
-        pos = lookup[rand() % (size - 1 + 1) + 1];
-
-        printf("\n");
-        printf("\n");
-
-        printf("%d\n", pos);
-
-        printf("\n");
-
-        if (lookup[pos] == pos)
-        {
-            for (int i = pos; i < size; i++)
-            {
-                lookup[i] = lookup[i + 1];
-            }
-            size--;
-        }
-        else if (lookup[pos] == pos - 1)
-        {
-            for (int i = pos - 1; i < size; i++)
-            {
-                lookup[i] = lookup[i + 1];
-            }
-            size--;
-        }
-        else
-        {
-            for (int i = pos - 1; i < size; i++)
-            {
-                lookup[i] = lookup[i + 1];
-            }
-            size--;
-        }
-    }
-
-
-    return 0;
+    printf("%c\n", randomNumber);
 }
